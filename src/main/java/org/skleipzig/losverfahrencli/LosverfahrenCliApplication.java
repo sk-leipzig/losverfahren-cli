53a7cf98012964cbc63a53cae1f03024b70dc7b5
Stefan Küttner <sk-leipzig@t-online.de>
2023-08-19 20:37:58 +0200

    Initialisierung Maven Projekt mit Spring Boot (Spring-Shell) und Java 17

---
+++ src/main/java/org/skleipzig/losverfahrencli/LosverfahrenCliApplication.java
@@ -0,0 +1,13 @@
+package org.skleipzig.losverfahrencli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LosverfahrenCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LosverfahrenCliApplication.class, args);
+	}
+
+}
